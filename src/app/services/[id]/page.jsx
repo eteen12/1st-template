12456d467b618a21b06464aa32ae91c64d6eaf31
eteen12 <eteensmash@gmail.com>
2024-11-01 16:59:27 -0700

    Update: Dynamic route working for services page by id

--- src/app/services/[id]/page.jsx
+++ src/app/services/[id]/page.jsx
@@ -1,9 +1,11 @@
+import { companyServices } from "@/components/pages/serviceContent";
 
-
-export default function Page(){
-    return(
-        <div>
-        
-        </div>
-    );
+export default async function Page({ params }) {
+  const { id } = await params;
+  const companyService = companyServices.find((p) => p.id === parseInt(id));
+  return (
+    <div>
+      <h1>{companyService.title}</h1>
+    </div>
+  );
 }
