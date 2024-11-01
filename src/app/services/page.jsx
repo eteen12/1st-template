ded8f26e2b828112143a59f8f7301a8638f6ba62
ethan <eteensmash@gmail.com>
2024-11-01 11:36:22 -0700

    card styling for services complete, to add react icons

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -10,7 +10,21 @@ export default function Page() {
     },
     {
       id: 2,
-      title: "Project Two, Kelowna, BC",
+      title: "Project Two, This Place",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui magni dicta cupiditate velit quos, hic molestias eum repellat.",
+      imageUrl: "path/to/image2.jpg", // Replace with actual image URL
+    },
+    {
+      id: 3,
+      title: "Project Two, Yeah this is something",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui magni dicta cupiditate velit quos, hic molestias eum repellat.",
+      imageUrl: "path/to/image2.jpg", // Replace with actual image URL
+    },
+    {
+      id: 4,
+      title: "Project Two, Yup, here too",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui magni dicta cupiditate velit quos, hic molestias eum repellat.",
       imageUrl: "path/to/image2.jpg", // Replace with actual image URL
@@ -21,13 +35,16 @@ export default function Page() {
   return (
     <div className="h-full w-full mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-36">
       <div className="max-w-2xl lg:mx-0 mb-10 mt-10 lg:flex-row lg:flex lg:max-w-7xl lg:justify-between ">
-        <div className="lg:max-w-lg">
-          <h2 className="text-base font-semibold leading-7 text-indigo-600">
+        <div className="lg:max-w-lg mx-auto text-center">
+          <h2 className="text-base lg:text- font-semibold leading-7 text-indigo-600">
             Projects
           </h2>
-          <p className="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
-            Our projects speak for themselves.
-          </p>
+          <h2 className="text-balance text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+               Our Services
+              </h2>
+          <p className="mt-2 text-lg/8 text-gray-600">
+                Aute magna irure deserunt veniam aliqua magna enim voluptate.
+              </p>
         </div>
       </div>
 
@@ -44,14 +61,15 @@ export default function Page() {
             ></div>
 
             {/* White content area with padding and shadow */}
-            <div className="relative bg-white px-8 py-8 shadow-lg mx-auto mb-10 z-20 ring-1 ring-gray-200 ">
+            <div className="relative bg-white px-8 py-8 shadow-lg mx-auto mb-10 z-20 ring-1 ring-gray-200 border-b-8 border-indigo-500">
+              <div className="absolute -top-10 w-16 h-16 bg-indigo-500 "></div>
               <h3 className="text-2xl font-bold">{project.title}</h3>
 
               <p className="text-lg leading-8 text-gray-600 mt-2">
                 {project.description}
               </p>
 
-              <div className="w-full border-t border-gray-300 mt-6" />
+             
             </div>
           </div>
         ))}
